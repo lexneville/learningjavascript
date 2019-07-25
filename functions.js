ad98cf208b17424769539c07f3671f490c2540b6
Code Nation 110 <codenation110@CN0110.local>
2019-07-25 10:06:16 +0100

    update

--- functions.js
+++ functions.js
@@ -12,7 +12,7 @@ const pressGrindBeans = () => {
 };
 
 pressGrindBeans();
-*/
+
 
 //Activity
 
@@ -21,3 +21,15 @@ const coffeeOrder = (size, drinkType) => {
 };
 
 coffeeOrder("medium", "cappucino");
+
+
+
+let accnumber = 50449921;
+const cashWithdrawal = (amount, accnum) => {
+ console.log(`Withdrawing ${amount} from account ${accnum}`);
+}
+cashWithdrawal(300, accnumber);
+cashWithdrawal(30, 50449921);
+cashWithdrawal(200, 50447921); 
+
+*/
