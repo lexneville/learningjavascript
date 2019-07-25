f6c3fe9fbc18c2e72643f2801824bffb5c10539d
Code Nation 110 <codenation110@CN0110.local>
2019-07-25 16:53:28 +0100

    added loops.js

--- loops.js
+++ loops.js
@@ -1,1 +1,19 @@
-for( i = 0 ; )
+//Loops
+/*
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//Reworking the above code to count down instead of up.
+
+for (i = 9; i >= 0; i--) {
+  console.log(i);
+}
+
+*/
+
+let favouriteDrinks = ["Pepsi Max", "Pepsi Max Cherry", "Pepsi Max Raspberry"];
+
+console.log(favouriteDrinks[0]);
+console.log(favouriteDrinks[1]);
+console.log(favouriteDrinks[2]);
